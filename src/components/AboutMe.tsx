ae5e17e87934d5b5b7a1e3471c41ebe304e4f28b
galin <galin-ka1@hotmail.com>
2025-09-01 19:43:43 +0200

    using in About me - heroKeys -(keyof HeroInfo)

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,17 +1,27 @@
-import {useEffect, useState} from 'react';
-import {period} from "../utils/constants.ts";
-import {characters} from "../utils/characters.ts";
-import {useParams} from "react-router";
+import { useEffect, useState } from "react";
+import { period } from "../utils/constants.ts";
+import { characters } from "../utils/characters.ts";
+import { useParams } from "react-router";
 
 interface HeroInfo {
-    name: string,
-    height: string,
-    birth_year: string,
-    gender: string,
-    mass: string,
-    skin_color: string,
-    eye_color: string,
+    name: string;
+    height: string;
+    birth_year: string;
+    gender: string;
+    mass: string;
+    skin_color: string;
+    eye_color: string;
 }
+
+const heroKeys: (keyof HeroInfo)[] = [
+    "name",
+    "height",
+    "birth_year",
+    "gender",
+    "mass",
+    "skin_color",
+    "eye_color",
+];
 
 const AboutMe = () => {
     const [hero, setHero] = useState<HeroInfo>({
@@ -23,43 +33,32 @@ const AboutMe = () => {
         skin_color: "",
         eye_color: "",
     });
-    const {heroId} = useParams();
+
+    const { heroId } = useParams();
 
     useEffect(() => {
         if (heroId != null) {
-            const heroStorage = JSON.parse(localStorage.getItem(heroId)!);
-            if (heroStorage && (Date.now() - heroStorage.time) < period) {
-                setHero(heroStorage.payload)
+            const heroStorage = JSON.parse(localStorage.getItem(heroId) ?? "null");
+            if (heroStorage && Date.now() - heroStorage.time < period) {
+                setHero(heroStorage.payload);
             } else {
                 fetch(characters[heroId].url)
-                    .then(response => response.json())
-                    .then(data => {
+                    .then((res) => res.json())
+                    .then((data) => {
                         setHero(data);
-                        const info = {
-                            payload: data,
-                            time: Date.now()
-                        };
+                        const info = { payload: data, time: Date.now() };
                         localStorage.setItem(heroId, JSON.stringify(info));
-                    })
+                    });
             }
         }
-    }, [heroId])
+    }, [heroId]);
 
-    const heroLabels = [
-        ["Name", hero.name],
-        ["Height", hero.height],
-        ["Birth Year", hero.birth_year],
-        ["Gender", hero.gender],
-        ["Mass", hero.mass],
-        ["Skin color", hero.skin_color],
-        ["Eye color", hero.eye_color],
-    ];
     return (
-        <div className="flex items-center justify-center ">
-            <div className="text-center text-2xl w-1/2 border ">
-                {heroLabels.map(([label, value]) => (
-                    <p key={label} >
-                        {label}: {value}
+        <div className="flex items-center justify-center m-1">
+            <div className="text-center text-2xl w-1/2 border border-white ">
+                {heroKeys.map((key) => (
+                    <p key={key} >
+                        {key.replace(/_/g, " ")}: {hero[key]}
                     </p>
                 ))}
             </div>
@@ -68,3 +67,5 @@ const AboutMe = () => {
 };
 
 export default AboutMe;
+
+
